7e94fd36b69493e3cc90c6565bf9812276200bca
AbelSu <okay9527@126.com>
2017-07-06 20:56:52 +0800

    Initial Commit

---
+++ SuCoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuCoreML
+//
+//  Created by abelsu on 2017/7/6.
+//  Copyright © 2017年 abelsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
